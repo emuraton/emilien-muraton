42cd58be64bb81b46abe39d08810095b9ea3797f
Emilien <emilien.muraton@gmail.com>
2019-08-22 12:04:52 +0100

    update: intro desktop css

--- src/components/Intro/styles.js
+++ src/components/Intro/styles.js
@@ -3,26 +3,29 @@ import styled from 'styled-components';
 //TODO remove it later
 export const Container = styled.section`
   display: flex;
-  background-image: linear-gradient(transparent 95%, #663399 0);
+  height: 100vh;
+  background-image: linear-gradient(transparent 90%, #663399 0);
   color: black;
 `;
 export const MarginWrapper = styled.div`
-  margin: 0 10%;
+  margin: 4% 10% 0 10%;
 `;
 
 export const Heading = styled.h1`
   font-size: 5rem;
   font-weight: 800;
-  margin-bottom: 1.5rem;
+  margin-bottom: 1.8rem;
 `;
 
 export const Description = styled.p`
   max-width: 800px;
   font-size: 1.6rem;
   font-weight: 300;
-  margin: 0 0 1rem 0;
+  margin: 0 0 1.5rem 0;
 `;
 
 export const EiffelTowerWrapper = styled.div`
-  margin-top: 300px;
+  position: fixed;
+  bottom: 32%;
+  right: 10%;
 `;
